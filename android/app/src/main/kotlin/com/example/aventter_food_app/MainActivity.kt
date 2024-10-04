8076e78197f4891ebecf3054454a65246cd1826e
Siméon DAOUDA <simeondaouda@gmail.com>
2024-10-04 15:38:59 +0100

    First commit initialisation

---
+++ android/app/src/main/kotlin/com/example/aventter_food_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aventter_food_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,354 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsIllustrationsGen {
+  const $AssetsIllustrationsGen();
+
+  /// File path: assets/illustrations/FRIED_RICE.webp
+  AssetGenImage get friedRice =>
+      const AssetGenImage('assets/illustrations/FRIED_RICE.webp');
+
+  /// File path: assets/illustrations/beer.png
+  AssetGenImage get beer =>
+      const AssetGenImage('assets/illustrations/beer.png');
+
+  /// File path: assets/illustrations/breakfast.png
+  AssetGenImage get breakfast =>
+      const AssetGenImage('assets/illustrations/breakfast.png');
+
+  /// File path: assets/illustrations/burger.png
+  AssetGenImage get burger =>
+      const AssetGenImage('assets/illustrations/burger.png');
+
+  /// File path: assets/illustrations/cheese.png
+  AssetGenImage get cheese =>
+      const AssetGenImage('assets/illustrations/cheese.png');
+
+  /// File path: assets/illustrations/chicken-kfc.png
+  AssetGenImage get chickenKfc =>
+      const AssetGenImage('assets/illustrations/chicken-kfc.png');
+
+  /// File path: assets/illustrations/chicken.png
+  AssetGenImage get chicken =>
+      const AssetGenImage('assets/illustrations/chicken.png');
+
+  /// File path: assets/illustrations/dinner.png
+  AssetGenImage get dinner =>
+      const AssetGenImage('assets/illustrations/dinner.png');
+
+  /// File path: assets/illustrations/dinner2.png
+  AssetGenImage get dinner2 =>
+      const AssetGenImage('assets/illustrations/dinner2.png');
+
+  /// File path: assets/illustrations/hot-dog.png
+  AssetGenImage get hotDog =>
+      const AssetGenImage('assets/illustrations/hot-dog.png');
+
+  /// File path: assets/illustrations/hot-soup.png
+  AssetGenImage get hotSoup =>
+      const AssetGenImage('assets/illustrations/hot-soup.png');
+
+  /// File path: assets/illustrations/lasagna.png
+  AssetGenImage get lasagna =>
+      const AssetGenImage('assets/illustrations/lasagna.png');
+
+  /// File path: assets/illustrations/legume.png
+  AssetGenImage get legume =>
+      const AssetGenImage('assets/illustrations/legume.png');
+
+  /// File path: assets/illustrations/lunch.png
+  AssetGenImage get lunch =>
+      const AssetGenImage('assets/illustrations/lunch.png');
+
+  /// File path: assets/illustrations/pasta.png
+  AssetGenImage get pasta =>
+      const AssetGenImage('assets/illustrations/pasta.png');
+
+  /// File path: assets/illustrations/pizza.png
+  AssetGenImage get pizza =>
+      const AssetGenImage('assets/illustrations/pizza.png');
+
+  /// File path: assets/illustrations/porridge.png
+  AssetGenImage get porridge =>
+      const AssetGenImage('assets/illustrations/porridge.png');
+
+  /// File path: assets/illustrations/ramen.png
+  AssetGenImage get ramen =>
+      const AssetGenImage('assets/illustrations/ramen.png');
+
+  /// File path: assets/illustrations/ramen2.png
+  AssetGenImage get ramen2 =>
+      const AssetGenImage('assets/illustrations/ramen2.png');
+
+  /// File path: assets/illustrations/rice.png
+  AssetGenImage get rice =>
+      const AssetGenImage('assets/illustrations/rice.png');
+
+  /// File path: assets/illustrations/rice2.png
+  AssetGenImage get rice2 =>
+      const AssetGenImage('assets/illustrations/rice2.png');
+
+  /// File path: assets/illustrations/salad.png
+  AssetGenImage get salad =>
+      const AssetGenImage('assets/illustrations/salad.png');
+
+  /// File path: assets/illustrations/salad2.png
+  AssetGenImage get salad2 =>
+      const AssetGenImage('assets/illustrations/salad2.png');
+
+  /// File path: assets/illustrations/sausage.png
+  AssetGenImage get sausage =>
+      const AssetGenImage('assets/illustrations/sausage.png');
+
+  /// File path: assets/illustrations/soft.png
+  AssetGenImage get soft =>
+      const AssetGenImage('assets/illustrations/soft.png');
+
+  /// File path: assets/illustrations/spaguetti.png
+  AssetGenImage get spaguetti =>
+      const AssetGenImage('assets/illustrations/spaguetti.png');
+
+  /// File path: assets/illustrations/sparkling-wine.png
+  AssetGenImage get sparklingWine =>
+      const AssetGenImage('assets/illustrations/sparkling-wine.png');
+
+  /// File path: assets/illustrations/tea.png
+  AssetGenImage get tea => const AssetGenImage('assets/illustrations/tea.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        friedRice,
+        beer,
+        breakfast,
+        burger,
+        cheese,
+        chickenKfc,
+        chicken,
+        dinner,
+        dinner2,
+        hotDog,
+        hotSoup,
+        lasagna,
+        legume,
+        lunch,
+        pasta,
+        pizza,
+        porridge,
+        ramen,
+        ramen2,
+        rice,
+        rice2,
+        salad,
+        salad2,
+        sausage,
+        soft,
+        spaguetti,
+        sparklingWine,
+        tea
+      ];
+}
+
+class $AssetsPlatesGen {
+  const $AssetsPlatesGen();
+
+  /// File path: assets/plates/plate1.jpg
+  AssetGenImage get plate1 => const AssetGenImage('assets/plates/plate1.jpg');
+
+  /// File path: assets/plates/plate10.jpg
+  AssetGenImage get plate10 => const AssetGenImage('assets/plates/plate10.jpg');
+
+  /// File path: assets/plates/plate11.jpg
+  AssetGenImage get plate11 => const AssetGenImage('assets/plates/plate11.jpg');
+
+  /// File path: assets/plates/plate12.jpg
+  AssetGenImage get plate12 => const AssetGenImage('assets/plates/plate12.jpg');
+
+  /// File path: assets/plates/plate13.jpg
+  AssetGenImage get plate13 => const AssetGenImage('assets/plates/plate13.jpg');
+
+  /// File path: assets/plates/plate14.jpg
+  AssetGenImage get plate14 => const AssetGenImage('assets/plates/plate14.jpg');
+
+  /// File path: assets/plates/plate15.jpg
+  AssetGenImage get plate15 => const AssetGenImage('assets/plates/plate15.jpg');
+
+  /// File path: assets/plates/plate16.jpg
+  AssetGenImage get plate16 => const AssetGenImage('assets/plates/plate16.jpg');
+
+  /// File path: assets/plates/plate17.jpg
+  AssetGenImage get plate17 => const AssetGenImage('assets/plates/plate17.jpg');
+
+  /// File path: assets/plates/plate18.jpg
+  AssetGenImage get plate18 => const AssetGenImage('assets/plates/plate18.jpg');
+
+  /// File path: assets/plates/plate19.jpg
+  AssetGenImage get plate19 => const AssetGenImage('assets/plates/plate19.jpg');
+
+  /// File path: assets/plates/plate2.jpg
+  AssetGenImage get plate2 => const AssetGenImage('assets/plates/plate2.jpg');
+
+  /// File path: assets/plates/plate20.jpg
+  AssetGenImage get plate20 => const AssetGenImage('assets/plates/plate20.jpg');
+
+  /// File path: assets/plates/plate21.jpg
+  AssetGenImage get plate21 => const AssetGenImage('assets/plates/plate21.jpg');
+
+  /// File path: assets/plates/plate22.jpg
+  AssetGenImage get plate22 => const AssetGenImage('assets/plates/plate22.jpg');
+
+  /// File path: assets/plates/plate23.jpg
+  AssetGenImage get plate23 => const AssetGenImage('assets/plates/plate23.jpg');
+
+  /// File path: assets/plates/plate24.jpg
+  AssetGenImage get plate24 => const AssetGenImage('assets/plates/plate24.jpg');
+
+  /// File path: assets/plates/plate3.jpg
+  AssetGenImage get plate3 => const AssetGenImage('assets/plates/plate3.jpg');
+
+  /// File path: assets/plates/plate4.jpg
+  AssetGenImage get plate4 => const AssetGenImage('assets/plates/plate4.jpg');
+
+  /// File path: assets/plates/plate5.jpg
+  AssetGenImage get plate5 => const AssetGenImage('assets/plates/plate5.jpg');
+
+  /// File path: assets/plates/plate6.jpg
+  AssetGenImage get plate6 => const AssetGenImage('assets/plates/plate6.jpg');
+
+  /// File path: assets/plates/plate7.jpg
+  AssetGenImage get plate7 => const AssetGenImage('assets/plates/plate7.jpg');
+
+  /// File path: assets/plates/plate8.jpg
+  AssetGenImage get plate8 => const AssetGenImage('assets/plates/plate8.jpg');
+
+  /// File path: assets/plates/plate9.jpg
+  AssetGenImage get plate9 => const AssetGenImage('assets/plates/plate9.jpg');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        plate1,
+        plate10,
+        plate11,
+        plate12,
+        plate13,
+        plate14,
+        plate15,
+        plate16,
+        plate17,
+        plate18,
+        plate19,
+        plate2,
+        plate20,
+        plate21,
+        plate22,
+        plate23,
+        plate24,
+        plate3,
+        plate4,
+        plate5,
+        plate6,
+        plate7,
+        plate8,
+        plate9
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsIllustrationsGen illustrations =
+      $AssetsIllustrationsGen();
+  static const AssetGenImage me = AssetGenImage('assets/me.png');
+  static const $AssetsPlatesGen plates = $AssetsPlatesGen();
+
+  /// List of all assets
+  static List<AssetGenImage> get values => [me];
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
